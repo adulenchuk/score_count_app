6e1d736ff57bc1454e314d2c182895e7732942be
adulenchuk <andrii.dulenchuk@gmail.com>
2024-11-11 01:54:24 +0200

    added button

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let team1Score = +document.querySelector("#team1_score p").innerHTML;
+let team2Score = +document.querySelector("#team2_score p").innerHTML;
+
 const button = document.getElementById("point1");
 button.onclick = () => {
   team1Score++;
