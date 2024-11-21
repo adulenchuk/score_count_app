e159364a4ac7f6d703822e8b21f6b704d99552fe
adulenchuk <andrii.dulenchuk@gmail.com>
2024-11-21 23:17:19 +0200

    removed few comments

--- script.js
+++ script.js
@@ -8,26 +8,12 @@ const scoreAElement = document.getElementById("scoreA");
 const scoreBElement = document.getElementById("scoreB");
 const timerElement = document.getElementById("timer");
 
-//Зміна рахунку
-document.addEventListener("click", (event) => {
-  const { team, points } = event.target.dataset;
-  if (team && points) {
-    const pointsValue = parseInt(points);
-    if (team === "A") {
-      scoreA += pointsValue;
-      scoreAElement.textContent = scoreA;
-    } else if (team === "B") {
-      scoreB += pointsValue;
-      scoreBElement.textContent = scoreB;
-    }
-  }
-});
 
 // Функція для оновлення відображення часу
 function updateTimeDisplay() {
   const minutes = Math.floor(remainingSeconds / 60)
-    .toString()
-    .padStart(2, "0");
+  .toString()
+  .padStart(2, "0");
   const seconds = (remainingSeconds % 60).toString().padStart(2, "0");
   timerElement.textContent = `${minutes}:${seconds}`;
 }
@@ -54,11 +40,22 @@ function pauseTimer() {
   timerInterval = null;
 }
 
-// Ініціалізуємо початковий час на дисплеї
 updateTimeDisplay();
 
-// Додаємо обробники для таймера
-document
-  .getElementById("startTimer")
-  .addEventListener("click", startCountdownTimer);
+document.getElementById("startTimer").addEventListener("click", startCountdownTimer);
 document.getElementById("pauseTimer").addEventListener("click", pauseTimer);
+
+//Зміна рахунку
+document.addEventListener("click", (event) => {
+  const { team, points } = event.target.dataset;
+  if (team && points) {
+    const pointsValue = parseInt(points);
+    if (team === "A") {
+      scoreA += pointsValue;
+      scoreAElement.textContent = scoreA;
+    } else if (team === "B") {
+      scoreB += pointsValue;
+      scoreBElement.textContent = scoreB;
+    }
+  }
+});
