88d526ad96ead9c6b1abd14fcd4d7fa2246a7ee2
adulenchuk <andrii.dulenchuk@gmail.com>
2024-12-01 03:40:13 +0200

    renamed html and js files, minor HTML changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-window.addEventListener("load", loadSavedGames);
-const gamesContainer = document.getElementById("gamesList"); // Передбачається, що є контейнер для списку
+const gamesContainer = document.getElementById("gamesList");
 
+window.addEventListener("load", loadSavedGames);
 
 function loadSavedGames() {
   const savedGames = JSON.parse(localStorage.getItem("basketballGames")) || [];
@@ -9,19 +9,15 @@ function loadSavedGames() {
     const gameHTML = `
         <div class="game">
           <h3>Game ${index + 1} - ${game.timestamp}</h3>
-          <p>Team A: ${game.scoreA} points, Fouls: ${
-      game.foulCountA
-    }, Jerseys: ${game.jerseyTypeA}</p>
-          <p>Team B: ${game.scoreB} points, Fouls: ${
-      game.foulCountB
-    }, Jerseys: ${game.jerseyTypeB}</p>
-        </div>
-      `;
+          <p>Team ${game.jerseyTypeA}: ${game.scoreA} points, ${game.foulCountA} fouls</p>
+          <p>Team ${game.jerseyTypeB}: ${game.scoreB} points, ${game.foulCountB} fouls</p>
+        </div>`
+        ;
     gamesContainer.insertAdjacentHTML("beforeend", gameHTML);
   });
 }
 
-document.getElementById("clearGames").addEventListener("click", () => {
+document.getElementById("resetSession").addEventListener("click", () => {
   localStorage.removeItem("basketballGames");
   gamesContainer.innerHTML = "";
   alert("All games cleared!");
